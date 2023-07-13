9f9a36ed68246e6ff05c91fdb21e7e7b4ebe1c65
Kamil Kaminski <kamilkaminski39@gmail.com>
2023-07-13 03:17:08 +0200

    Added Knight, Bishop, Rook Queen and King listing available moves logic

--- app/figures.py
+++ app/figures.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from consts import valid_columns
+from consts import valid_columns, valid_king_moves, valid_knight_moves
 
 
 class Figure:
@@ -48,103 +48,158 @@ class Pawn(Figure):
 
 
 class Knight(Figure):
-    def list_available_moves(self):
-        pass
+    def list_available_moves(self) -> List[str]:
+        super().list_available_moves()
 
-    def validate_move(self, dest_field):
-        pass
+        for move in valid_knight_moves:
+            next_col_index = self.column_index + move[0]
+            if 0 <= next_col_index < len(valid_columns):
+                next_col = valid_columns[next_col_index]
+                next_row = self.current_row + move[1]
+                if 1 <= next_row <= 8:
+                    self.available_moves.append(f"{next_col}{next_row}")
+
+        return self.available_moves
 
 
 class Bishop(Figure):
-    def list_available_moves(self):
-        pass
+    def list_available_moves(self) -> List[str]:
+        super().list_available_moves()
+        range_end = len(valid_columns) - self.column_index
 
-    def validate_move(self, dest_field):
-        pass
+        # Diagonal moves towards top-right
+        for i in range(1, range_end):
+            next_col = valid_columns[self.column_index + i]
+            next_row = self.current_row + i
+            if next_col is not None and next_row <= 8:
+                self.available_moves.append(f"{next_col}{next_row}")
+            else:
+                break
+
+        # Diagonal moves towards top-left
+        for i in range(1, self.column_index + 1):
+            prev_col = valid_columns[self.column_index - i]
+            next_row = self.current_row + i
+            if prev_col is not None and next_row <= 8:
+                self.available_moves.append(f"{prev_col}{next_row}")
+            else:
+                break
+
+        # Diagonal moves towards bottom-right
+        for i in range(1, range_end):
+            next_col = valid_columns[self.column_index + i]
+            prev_row = self.current_row - i
+            if next_col is not None and prev_row >= 1:
+                self.available_moves.append(f"{next_col}{prev_row}")
+            else:
+                break
+
+        # Diagonal moves towards bottom-left
+        for i in range(1, self.column_index + 1):
+            prev_col = valid_columns[self.column_index - i]
+            prev_row = self.current_row - i
+            if prev_col is not None and prev_row >= 1:
+                self.available_moves.append(f"{prev_col}{prev_row}")
+            else:
+                break
+
+        return self.available_moves
 
 
 class Rook(Figure):
-    def list_available_moves(self):
-        pass
+    def list_available_moves(self) -> List[str]:
+        super().list_available_moves()
 
-    def validate_move(self, dest_field):
-        pass
+        # Moves towards the top of the board
+        for row in range(self.current_row + 1, 9):
+            self.available_moves.append(f"{self.current_column}{row}")
+
+        # Moves towards the bottom of the board
+        for row in range(self.current_row - 1, 0, -1):
+            self.available_moves.append(f"{self.current_column}{row}")
+
+        # Moves towards the right side of the board
+        for col in valid_columns[self.column_index + 1 :]:
+            self.available_moves.append(f"{col}{self.current_row}")
+
+        # Moves towards the left side of the board
+        for col in reversed(valid_columns[: self.column_index]):
+            self.available_moves.append(f"{col}{self.current_row}")
+
+        return self.available_moves
 
 
 class Queen(Figure):
     def list_available_moves(self):
-        pass
+        super().list_available_moves()
+        range_end = len(valid_columns) - self.column_index
 
-    def validate_move(self, dest_field):
-        pass
+        # Moves towards the top of the board
+        for row in range(self.current_row + 1, 9):
+            self.available_moves.append(f"{self.current_column}{row}")
+
+        # Moves towards the bottom of the board
+        for row in range(self.current_row - 1, 0, -1):
+            self.available_moves.append(f"{self.current_column}{row}")
+
+        # Moves towards the right side of the board
+        for col in valid_columns[self.column_index + 1 :]:
+            self.available_moves.append(f"{col}{self.current_row}")
+
+        # Moves towards the left side of the board
+        for col in reversed(valid_columns[: self.column_index]):
+            self.available_moves.append(f"{col}{self.current_row}")
+
+        # Diagonal moves towards top-right
+        for i in range(1, range_end):
+            next_col = valid_columns[self.column_index + i]
+            next_row = self.current_row + i
+            if next_col is not None and next_row <= 8:
+                self.available_moves.append(f"{next_col}{next_row}")
+            else:
+                break
+
+        # Diagonal moves towards top-left
+        for i in range(1, self.column_index + 1):
+            prev_col = valid_columns[self.column_index - i]
+            next_row = self.current_row + i
+            if prev_col is not None and next_row <= 8:
+                self.available_moves.append(f"{prev_col}{next_row}")
+            else:
+                break
+
+        # Diagonal moves towards bottom-right
+        for i in range(1, range_end):
+            next_col = valid_columns[self.column_index + i]
+            prev_row = self.current_row - i
+            if next_col is not None and prev_row >= 1:
+                self.available_moves.append(f"{next_col}{prev_row}")
+            else:
+                break
+
+        # Diagonal moves towards bottom-left
+        for i in range(1, self.column_index + 1):
+            prev_col = valid_columns[self.column_index - i]
+            prev_row = self.current_row - i
+            if prev_col is not None and prev_row >= 1:
+                self.available_moves.append(f"{prev_col}{prev_row}")
+            else:
+                break
+
+        return self.available_moves
 
 
 class King(Figure):
     def list_available_moves(self) -> List[str]:
         super().list_available_moves()
 
-        # For the A column and 1 row
-        if self.current_column == valid_columns[0] and self.current_row == 1:
-            self.available_moves.append(f"{self.current_column}{self.next_row}")
-            self.available_moves.append(f"{self.next_column}{self.next_row}")
-            self.available_moves.append(f"{self.next_column}{self.current_row}")
-
-        # For the H column and 1 row
-        if self.current_column == valid_columns[7] and self.current_row == 1:
-            self.available_moves.append(f"{self.current_column}{self.next_row}")
-            self.available_moves.append(f"{self.previous_column}{self.next_row}")
-            self.available_moves.append(f"{self.previous_column}{self.current_row}")
-
-        # For the 1st row except the A column and H column
-        if (
-            self.current_row == 1
-            and self.current_column != valid_columns[0]
-            and self.current_column != valid_columns[7]
-        ):
-            self.available_moves.append(f"{self.previous_column}{self.current_row}")
-            self.available_moves.append(f"{self.previous_column}{self.next_row}")
-            self.available_moves.append(f"{self.current_column}{self.next_row}")
-            self.available_moves.append(f"{self.next_column}{self.next_row}")
-            self.available_moves.append(f"{self.next_column}{self.current_row}")
-
-        # For the 8th row except the A column and H column
-        if (
-            self.current_row == 8
-            and self.current_column != valid_columns[0]
-            and self.current_column != valid_columns[7]
-        ):
-            self.available_moves.append(f"{self.previous_column}{self.current_row}")
-            self.available_moves.append(f"{self.previous_column}{self.previous_row}")
-            self.available_moves.append(f"{self.current_column}{self.previous_row}")
-            self.available_moves.append(f"{self.next_column}{self.previous_row}")
-            self.available_moves.append(f"{self.next_column}{self.current_row}")
-
-        # For the A column except the 1st row and 8th row
-        if self.column_index == 0 and self.current_row not in [1, 8]:
-            self.available_moves.append(f"{self.current_column}{self.next_row}")
-            self.available_moves.append(f"{self.next_column}{self.next_row}")
-            self.available_moves.append(f"{self.next_column}{self.current_row}")
-            self.available_moves.append(f"{self.next_column}{self.previous_row}")
-            self.available_moves.append(f"{self.current_column}{self.previous_row}")
-
-        # For the H column except the 1st row and 8th row
-        if self.column_index == 7 and self.current_row not in [1, 8]:
-            self.available_moves.append(f"{self.current_column}{self.next_row}")
-            self.available_moves.append(f"{self.previous_column}{self.next_row}")
-            self.available_moves.append(f"{self.previous_column}{self.current_row}")
-            self.available_moves.append(f"{self.previous_column}{self.previous_row}")
-            self.available_moves.append(f"{self.current_column}{self.previous_row}")
-
-        # For the rest of columns
-        if self.column_index not in [0, 7] and self.current_row not in [1, 8]:
-            self.available_moves.append(f"{self.current_column}{self.previous_row}")
-            self.available_moves.append(f"{self.previous_column}{self.previous_row}")
-            self.available_moves.append(f"{self.previous_column}{self.current_row}")
-            self.available_moves.append(f"{self.previous_column}{self.next_row}")
-            self.available_moves.append(f"{self.current_column}{self.next_row}")
-            self.available_moves.append(f"{self.next_column}{self.next_row}")
-            self.available_moves.append(f"{self.next_column}{self.current_row}")
-            self.available_moves.append(f"{self.next_column}{self.previous_row}")
+        for move in valid_king_moves:
+            next_col_index = self.column_index + move[0]
+            if 0 <= next_col_index < len(valid_columns):
+                next_col = valid_columns[next_col_index]
+                next_row = self.current_row + move[1]
+                if 1 <= next_row <= 8:
+                    self.available_moves.append(f"{next_col}{next_row}")
 
         return self.available_moves
 
